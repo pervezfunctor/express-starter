b6a75c1e8e51bb02a22da03da0e7ed30bafbab8b
Pervez Iqbal <pervezfunctor@gmail.com>
2019-04-19 22:07:09 +0530

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+
+const port = 3000
+
+const app = (() => {
+  const app = express()
+  app.use(bodyParser.json())
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`)
+  })
+
+  return app
+})()
+
+app.get('/hello', async (_, res) => {
+  res.send('hello, world!')
+})
