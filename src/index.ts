e16abad28ae7783da6da80a7739ead3a11345cdd
Pervez Iqbal <pervezfunctor@gmail.com>
2019-05-18 21:57:44 +0530

    add cors middleware

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,13 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const port = 3000
 
 const app = (() => {
   const app = express()
   app.use(bodyParser.json())
-
+  app.use(cors({ methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'] }))
   app.listen(port, () => {
     console.log(`Example app listening on port ${port}!`)
   })
